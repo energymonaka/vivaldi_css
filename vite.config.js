1f8777878eaaa62d3f6db02fd3d2329624489777
zprl <zprl.dev@gmail.com>
2023-10-29 03:10:23 +0900

    update

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import UnoCSS from "unocss/vite";
+
+const root = "src";
+const dir = "./" + root;
+
+process.env.BROWSER =
+  "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe";
+
+export default defineConfig({
+  base: "/",
+  root: root,
+  plugins: [UnoCSS()],
+  server: {
+    open: "index.html",
+  },
+});
