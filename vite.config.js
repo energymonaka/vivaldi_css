4bd0e65afb173efcf012ecb4246ea05312b9b5ab
zprl <zprl.dev@gmail.com>
2023-10-29 07:55:24 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -17,5 +17,10 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     assetsDir: "./assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
 });
